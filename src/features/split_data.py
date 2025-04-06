87adb03d30fae2120b076fa215d9fc3b27e3d31c
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-04-06 23:05:18 +0530

    model building

---
+++ src/features/split_data.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+import os
+import src.utils as utils
+
+# Logging configuration
+logger = utils.configure_logger(__name__, log_file="splitting_data.log")
+
+def load_data(file_path: str) -> pd.DataFrame:
+    try:
+        logger.debug("Loading Data")
+        df = pd.read_csv(file_path)
+        logger.info("Data loaded successfully")
+        return df
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        return pd.DataFrame()
+
+if __name__ == "__main__":
+    try:
+        # loading params
+        params = utils.load_params("params.yaml", "split_data", logger)
+
+        # load data
+        data_path = os.path.join("data", "processed")
+        file_path = os.path.join(data_path, "gurgaon_properties_post_feature_selection.csv")
+        df = load_data(file_path)
+        if df.empty:
+            raise ValueError("Data loading failed: Empty DataFrame")
+        
+        # split data into train and test
+        train, test = train_test_split(df, test_size=params["test_size"], random_state=params["random_state"])
+
+        # save train and test data
+        data_path = os.path.join("data", "processed")
+        utils.save_data(train, data_path, "train.csv", logger=logger)
+        utils.save_data(test, data_path, "test.csv", logger=logger)
+
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
---
+++ src/models/model_building.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+
+import os
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+
+import category_encoders as ce
+
+import pickle
+
+import src.utils as utils
+
+# Logging configuration
+logger = utils.configure_logger(__name__, log_file="model_building.log")
+
+def load_data(file_path: str) -> pd.DataFrame:
+    try:
+        logger.debug("Loading Data")
+        df = pd.read_csv(file_path)
+        logger.info("Data loaded successfully")
+        return df
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        return pd.DataFrame()
+
+
+if __name__ == "__main__":
+    try:
+        # loading params
+        params = utils.load_params("params.yaml", "model_building", logger)
+
+        # load data
+        data_path = os.path.join("data", "processed")
+        file_path = os.path.join(data_path, "train.csv")
+        df = load_data(file_path)
+        if df.empty:
+            raise ValueError("Data loading failed: Empty DataFrame")
+        
+        X = df.drop(columns=['price'])
+        y = df['price']
+
+        # Applying log transform
+        logger.info("Applying log transform")
+        y_transformed = np.log1p(y)
+
+        columns_to_encode = ['property_type', 'sector', 'balcony', 'agePossession', 'furnishing_type', 'luxury_category', 'floor_category']
+
+        # Create the pipeline
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', StandardScaler(), ['bedRoom', 'bathroom', 'built_up_area', 'servant room', 'store room']),
+                ('cat', OrdinalEncoder(), columns_to_encode),
+                ('cat1', OneHotEncoder(drop='first', sparse_output=False), ['agePossession']),
+                ('target_enc', ce.TargetEncoder(), ['sector'])
+            ],
+            remainder='passthrough'
+        )
+
+        model = RandomForestRegressor(
+            n_estimators=params["n_estimators"],
+            max_depth=params["max_depth"],
+            max_samples=params["max_samples"],
+            max_features=params["max_features"]
+        )
+
+        pipeline = Pipeline([
+            ("preprocessor", preprocessor),
+            ("regressor", model)
+        ])
+
+        logger.info("Fitting the model")
+        pipeline.fit(X, y_transformed)
+
+        # Save the pipeline
+        model_path = os.path.join("models")
+        os.makedirs(model_path, exist_ok=True)
+
+        with open(os.path.join(model_path, "real_estate_predictor.pkl"), 'wb') as file:
+            pickle.dump(pipeline, file)
+
+    except Exception as e:
+        logger.error(f"Error : {e}")
